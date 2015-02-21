3063827b3a689e6460e41577c64ac8f673a54960
Daniel <daniel65536@gmail.com>
2015-02-21 15:22:25 +0800

    lint code

--- src/make.py
+++ src/make.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def loadProxy(data):
+def load_proxy(data):
     lines = data.splitlines()
     return '"{}"'.format(';'.join(lines))
 
-def loadRange(data):
+def load_range(data):
     lines = data.splitlines()
+    lines.append('127.0.0.0/24')
+    lines.append('192.168.0.0/16')
+    lines.append('172.16.0.0/12')
+    lines.append('10.0.0.0/8')
 
     codelist = [[] for i in range(256)]
     masklist = [[] for i in range(256)]
 
     for line in lines:
-            item = line.split('/')
-            atom = item[0].split('.')
-            codelist[int(atom[0])].append(int(atom[1]) << 8 | int(atom[2]) << 0)
-            masklist[int(atom[0])].append(24 - int(item[1]))
+        item = line.split('/')
+        atom = item[0].split('.')
+        codelist[int(atom[0])].append(int(atom[1]) << 8 | int(atom[2]) << 0)
+        masklist[int(atom[0])].append(24 - int(item[1]))
 
     codelist = ['[{}]'.format(','.join(map(str, x))) for x in codelist]
     masklist = ['[{}]'.format(','.join(map(str, x))) for x in masklist]
@@ -25,30 +29,30 @@ def loadRange(data):
 
     return codelist, masklist
 
-
-def loadDomain(data):
+def load_domain(data):
     lines = filter(lambda x:not x.startswith('#') and x, data.splitlines())
+    lines.append('cn')
     domains = []
     for line in lines:
-        if sum(map(lambda x:line.endswith(x), lines)) == 1:
+        if sum(map(line.endswith, lines)) == 1:
             domains.append(line)
-    return '{{{}}}'.format(','.join(map(lambda x:'"{}":1'.format(x), domains)))
+    return '{{{}}}'.format(','.join(map('"{}":1'.format, domains)))
 
 def main():
     with open('mono.js') as f:
         payload = f.read()
 
     with open('proxyList') as f:
-        proxylist = loadProxy(f.read())
+        proxylist = load_proxy(f.read())
 
     with open('whiteList') as f:
-        whitelist = loadDomain(f.read())
+        whitelist = load_domain(f.read())
 
     with open('blackList') as f:
-        blacklist = loadDomain(f.read())
+        blacklist = load_domain(f.read())
 
     with open('ipList') as f:
-        codelist, masklist = loadRange(f.read())
+        codelist, masklist = load_range(f.read())
 
     payload = payload.replace('__proxyList__', proxylist)
     payload = payload.replace('__whiteList__', whitelist)
--- tools/apnic2iplist.py
+++ tools/apnic2iplist.py
@@ -5,10 +5,6 @@ with open('delegated-apnic-latest') as f:
     data = f.read()
 
 lines = data.splitlines()
-lines.append('apnic|CN|ipv4|127.0.0.0|256')
-lines.append('apnic|CN|ipv4|192.168.0.0|65536')
-lines.append('apnic|CN|ipv4|172.16.0.0|1048576')
-lines.append('apnic|CN|ipv4|10.0.0.0|16777216')
 
 codelist = [[] for i in range(256)]
 masklist = [[] for i in range(256)]
